130059ea3869ad6680f710336060ebccd2adfd16
kelly r maass <kmaass77627@gmail.com>
2019-10-14 22:18:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x = 10
+while x > 0 
+  puts "#{number} SECOND(S)!"
+  x -= 1
+  "HAPPY NEW YEAR!"
 end
+end
+
+
+
+
+
+def countdown_with_sleep(x)
+   x = 20
+while x > 0 
+  puts "#{number} SECOND(S)!"
+  x -= 1
+  sleep(1.seconds)
+end 
+end
+
+
+  
+  
